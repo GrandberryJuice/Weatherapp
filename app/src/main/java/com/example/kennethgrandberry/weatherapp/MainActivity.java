aa5789071e459c12442804991830fd4821fd469d
KENNETH GRANDBERRY <kennethgrandberry@kenneths-air.attlocal.net>
2016-12-26 18:23:31 -0600

    Parsed the data and update google play services

--- app/src/main/java/com/example/kennethgrandberry/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/kennethgrandberry/weatherapp/MainActivity.java
@@ -1,38 +1,153 @@
 package com.example.kennethgrandberry.weatherapp;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.renderscript.Double2;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.kennethgrandberry.weatherapp.model.DailyWeatherReport;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     //urls
     final String URL_Base = "http://api.openweathermap.org/data/2.5/forecast";
-    final String URL_COORD = "/?lat=35.105&lon=-89.840";
+    final String URL_COORD = "?lat=";//"/?lat=35.105&lon=-89.840";
     final String URL_UNITS = "&units=imperial";
     final String URL_API_KEY = "&APPID=7fba1493634df010704ff49eac7e30b4";
 
+    private GoogleApiClient mGoogleApiClient;
+    private final int Permission_Location = 111;
+    private ArrayList<DailyWeatherReport>weatherReportList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String url = URL_Base + URL_COORD +URL_UNITS + URL_API_KEY;
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this,this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        downloadWeatherData(location);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Permission_Location);
+        } else {
+            startLocationServices();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+
+    public void startLocationServices() {
+        try {
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,req,this);
+
+        }catch (SecurityException exception) {
+
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case Permission_Location: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationServices();
+                } else {
+                    //show a dialog saying something like, "I can't run your location dummy -you denied permission!
+                    Toast.makeText(this,"I can't run your location", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public void downloadWeatherData(Location location) {
+        final String fullCoords = URL_COORD + location.getLatitude() + "&lon" + location.getLongitude();
+        final String url = URL_Base + fullCoords +URL_UNITS + URL_API_KEY;
 
         final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-             Log.v("FUN","RES:" + response.toString());
+                Log.v("FUN","RES:" + response.toString());
 
+                try {
+
+                    JSONObject city = response.getJSONObject("city");
+                    String cityName = city.getString("name");
+                    String country = city.getString("country");
+                    Log.v("JSON","Name:" + cityName + "country" + country);
+
+                    JSONArray list = response.getJSONArray("list");
+
+                    for (int x = 0; x < 5; x++) {
+                        JSONObject obj = list.getJSONObject(x);
+                        JSONObject main = obj.getJSONObject("main");
+                        Double currentTemp = main.getDouble("temp");
+                        Double maxTemp = main.getDouble("temp_max");
+                        Double minTemp = main.getDouble("temp_min");
+
+                        JSONArray weatherArr = obj.getJSONArray("weather");
+                        JSONObject weather = weatherArr.getJSONObject(0);
+                        String weatherType = weather.getString("main");
+
+                        String rawDate = obj.getString("dt_txt");
+
+                        DailyWeatherReport report = new DailyWeatherReport(cityName, country, currentTemp.intValue(), maxTemp.intValue(), minTemp.intValue(),weatherType, rawDate);
+                        Log.v("JSON", "Printing from class:" + report.getWeather());
+                        weatherReportList.add(report);
+                    }
+
+                }catch (JSONException e){
+                    Log.v("Json", "EXC:" + e.getLocalizedMessage());
+                }
 
             }
         }, new Response.ErrorListener() {
@@ -45,4 +160,5 @@ public class MainActivity extends AppCompatActivity {
 
         Volley.newRequestQueue(this).add(jsonObjectRequest);
     }
+
 }
---
+++ app/src/main/java/com/example/kennethgrandberry/weatherapp/model/DailyWeatherReport.java
@@ -0,0 +1,59 @@
+package com.example.kennethgrandberry.weatherapp.model;
+
+/**
+ * Created by kennethgrandberry on 12/26/16.
+ */
+public class DailyWeatherReport {
+    private String cityName;
+    private String country;
+    private int currentTemp;
+    private int maxTemp;
+    private int minTemp;
+    private String weather;
+    private String formattedDate;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getCurrentTemp() {
+        return currentTemp;
+    }
+
+    public int getMaxTemp() {
+        return maxTemp;
+    }
+
+    public int getMinTemp() {
+        return minTemp;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public String getFormattedDate() {
+        return formattedDate;
+    }
+
+    public DailyWeatherReport(String cityName, String country, int currentTemp, int maxTemp, int minTemp, String weather, String rawDate) {
+        this.cityName = cityName;
+        this.country = country;
+        this.currentTemp = currentTemp;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.weather = weather;
+        this.formattedDate = rawDateToPretty(rawDate);
+    }
+
+
+    public String rawDateToPretty(String rawDate) {
+        //convert raw date into formatted date
+        return "May 1";
+    }
+
+}
